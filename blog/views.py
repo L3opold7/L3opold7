f452a9fd2c6c057bef34f5c09646da4b868c34b5
L3opold7 <l3opold7@gmail.com>
2015-11-29 12:34:02 +0900

    Add post_detail, modify post_list and urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from blog.models import Post
@@ -7,3 +7,7 @@ from blog.models import Post
 def post_list(request):
     posts = Post.objects.all().order_by('created_at')
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
