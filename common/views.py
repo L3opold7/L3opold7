228105cc068aa78b8bf06db36cb3752311685f1a
Myungseo Kang <l3opold7@gmail.com>
2016-07-28 16:25:39 +0900

    변경

--- common/views.py
+++ common/views.py
@@ -14,7 +14,7 @@ def register(request):
             user.set_password(password)
             user.save()
             return redirect('/login/')
-        except IntegrityError: #unique=True를 이용한 try ... except 코딩
+        except IntegrityError:  # unique=True를 이용한 try ... except 코딩
             results['error'] = 'Already Used!'
     return render(request, 'register.html', results)
 
@@ -22,11 +22,12 @@ def register(request):
 def login(request):
     results = {}
     if request.method == 'POST':
-        username = request.POST.get('username', '') #username이 있으면 할당, 없으면 두 번째 인자값으로 할당
+        username = request.POST.get('username', '')  # username이 있으면 할당, 없으면 두 번째 인자값으로 할당
         password = request.POST.get('password', '')
-        user = authenticate(username=username, password=password) #각각 username과 password를 찾아서 있으면 user객체를 반환, 없으면 null 반환
+        # 각각 username과 password를 찾아서 있으면 user객체를 반환, 없으면 null 반환
+        user = authenticate(username=username, password=password)
         if user is not None:
-            auth_login(request, user) #request와 user 객체를 넘겨줘서 실제 로그인을 시켜주는 메서드
+            auth_login(request, user)  # request와 user 객체를 넘겨줘서 실제 로그인을 시켜주는 메서드
             return redirect('/')
         else:
             results['error'] = 'Wrong! Try Again!'
