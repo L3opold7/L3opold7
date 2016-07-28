4fb63fb81bb409ca7478d8bd0a37b1b030c256d6
Ubuntu <ubuntu@ip-172-31-2-150.ap-northeast-2.compute.internal>
2016-07-28 00:14:14 +0000

    wsgi.py 수정

--- L3opold7/wsgi.py
+++ L3opold7/wsgi.py
@@ -1,16 +1,20 @@
 """
-WSGI config for L3opold7 project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import site
+import sys
 
-from django.core.wsgi import get_wsgi_application
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+site.addsitedir('/home/ubuntu/.pyenv/versions/denv/lib/python3.5/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "L3opold7.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
