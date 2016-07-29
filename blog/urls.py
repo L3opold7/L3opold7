4fdfeb06972dfb9b5ee653ce6294420431be9a5b
Myungseo Kang <l3opold7@gmail.com>
2016-07-29 17:36:49 +0900

    url 변경

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
-    url(r'^post/(?P<pk>[0-9]+)/photo/upload_photo$', views.upload_photo, name='upload_photo'),
+    url(r'^post/(?P<pk>[0-9]+)/upload_photo$', views.upload_photo, name='upload_photo'),
 ]
