2efb61394827bd87003fa44fc216917e2f77e9df
Myungseo Kang <l3opold7@gmail.com>
2016-07-29 17:05:21 +0900

    settings 분리 fail

--- Leopold/settings.py
+++ Leopold/settings.py
@@ -18,6 +18,14 @@ INSTALLED_APPS = (
     'storages',
 )
 
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +56,19 @@ TEMPLATES = [
     },
 ]
 
+# Django Pipeline
+PIPELINE = {
+    'PIPELINE_ENABLED': True,
+    'STYLESHEETS': {
+        'blog': {
+            'source_filename': (
+                'css/blog.css',
+            ),
+            'output_filename': 'css/blog.css'
+        }
+    }
+}
+
 WSGI_APPLICATION = 'Leopold.wsgi.application'
 
 
@@ -62,3 +83,36 @@ USE_L10N = True
 USE_TZ = True
 
 AUTH_USER_MODEL = "common.UserProfile"
+
+
+DEBUG = True
+
+if DEBUG:
+    ALLOWED_HOSTS = []
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+else:
+    ALLOWED_HOSTS = ['*']
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+    # AWS Setting
+    AWS_REGION = 'ap-northeast-2'
+    AWS_STORAGE_BUCKET_NAME = 'l3opold7'
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+    AWS_ACCESS_KEY_ID = 'AKIAJA7YZMQQ5GG7LTPQ'
+    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    # Static Setting
+    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+    # Media Setting
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
