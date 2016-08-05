65f7bb1278accd4acb7456ced117d77629aa7a10
Myungseo Kang <l3opold7@gmail.com>
2016-08-05 06:53:58 +0000

    static root º¯°

--- Leopold/settings.py
+++ Leopold/settings.py
@@ -92,7 +92,7 @@ DEBUG = True
 
 if DEBUG:
     STATIC_URL = '/static/'
-    STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
