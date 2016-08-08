6521b73f0c58deaa8b28f86f317de1ec9231478a
Myungseo Kang <l3opold7@gmail.com>
2016-08-08 03:28:08 +0000

    Add post archive templates

--- Leopold/settings.py
+++ Leopold/settings.py
@@ -97,6 +97,7 @@ SITE_ID = 1
 if DEBUG:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_admin')
 
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -106,17 +107,20 @@ else:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
     # AWS Setting
+    AWS_ACCESS_KEY_ID = 'AKIAJA7YZMQQ5GG7LTPQ'
+    AWS_SECRET_ACCESS_KEY = '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'
+
     AWS_REGION = 'ap-northeast-2'
     AWS_STORAGE_BUCKET_NAME = 'l3opold7'
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
-    AWS_ACCESS_KEY_ID = 'AKIAJA7YZMQQ5GG7LTPQ'
-    AWS_SECRET_ACCESS_KEY = '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'
+
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
     # Static Setting
-    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    STATIC_ROOT = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
     # Media Setting
     MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
